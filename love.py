09285235340af8944b23675e5c955804d3936e94
Priya <pooniapriya550@gmail.com>
2025-02-07 19:32:08 +0530

    edit the love.py

--- love.py
+++ love.py
@@ -2,5 +2,7 @@ import random
 a = random.randint(1,3)
 if a==2:
     print("priya loves me")
+elif a==1:
+    print("Aayush is going to die")
 else:
-    print("Aayush is her crush")
+    print("Aayush is her crush.....")
